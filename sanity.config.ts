9bdb07a11a246e510c3c0d6e71e4f56a611eacc0
mornir <jeromepott@hotmail.com>
2025-03-19 16:47:42 +0100

    feat: remove language filter

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'sanity'
-import { languageFilter } from '@sanity/language-filter'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './src/schemaTypes'
-import { supportedLanguages } from './src/schemaTypes/documents/article'
 
 // Environment variables for project configuration
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'your-projectID'
@@ -12,7 +10,7 @@ const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
 
 export default defineConfig({
   name: 'sanity-template-astro-clean',
-  title: 'Sanity Astro Starter',
+  title: 'BSV 2026',
   projectId,
   dataset,
   document: {
@@ -20,11 +18,7 @@ export default defineConfig({
       enabled: false,
     },
   },
-  plugins: [structureTool(), visionTool(), languageFilter({
-    supportedLanguages,
-    defaultLanguages: ['de'],
-    documentTypes: ['article'],
-  })],
+  plugins: [structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
   },
--- src/schemaTypes/index.ts
+++ src/schemaTypes/index.ts
@@ -1,7 +1,7 @@
 import blockContent from './objects/blockContent'
-import { localeString, localeBlockContent } from './objects/languages'
+/* import { localeString, localeBlockContent } from './objects/languages' */
 import { article } from './documents/article'
 
 // Export an array of all the schema types.  This is used in the Sanity Studio configuration. https://www.sanity.io/docs/schema-types
 
-export const schemaTypes = [article, blockContent, localeString, localeBlockContent]
+export const schemaTypes = [article, blockContent]

7fa408500c93faabba05ef54a4fab26d56c44ab9
mornir <jeromepott@hotmail.com>
2026-01-29 10:34:19 +0100

    fix: name variable

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -9,7 +9,7 @@ import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
-const appId = process.env.SANITY_STUDIO_DATASET || '<your app ID>'
+const appId = process.env.SANITY_STUDIO_APP_ID || '<your app ID>'
 
 export default defineCliConfig({
   api: {
